b3402ca4d12dc97cad9777043d462841de9c667c
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-03-02 15:49:53 +0900

    add max1000: not working because of lack of resources

---
+++ README_ja.md
@@ -0,0 +1,13 @@
+# SCRx SDK FPGAプロジェクトリポジトリ
+
+## Repository contents
+Folder | Description
+------ | -----------
+a5              | Projects for Intel Arria V GX FPGA Starter Kit
+arty            | Projects for Digilent's Arty board
+de10lite        | Projects for Terasic's DE10-Lite board
+max1000         | Arrow's MAX1000ボード（容量オーバーで実装できず）
+nexys4ddr       | Projects for Digilent's Nexys4DDR board
+hex             | Common HEX-files
+scripts/xilinx  | Scripts for Xilinx FPGA projects
+README.md       | This file
---
+++ max1000/ip/scr1_arch_custom.svh
@@ -0,0 +1,24 @@
+`ifndef SCR1_ARCH_CUSTOM_SVH
+`define SCR1_ARCH_CUSTOM_SVH
+/// Copyright by Syntacore LLC © 2016, 2017. See LICENSE for details
+/// @file       <scr1_arch_custom.svh>
+/// @brief      Custom Architecture Parameters File
+///
+
+`define SCR1_TARGET_FPGA_INTEL
+`define SCR1_ARCH_BUILD_ID          32'h19090900
+
+
+
+parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_RST_VECTOR        = 'hFFFFFF00;   // Reset vector
+parameter bit [`SCR1_XLEN-1:0]          SCR1_ARCH_CSR_MTVEC_BASE    = 'hFFFFFF80;   // MTVEC BASE field reset value
+
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_MASK          = 'hFFFF8000;   // TCM mask and size
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TCM_ADDR_PATTERN       = 'hF0000000;   // TCM address match pattern
+
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TIMER_ADDR_MASK        = 'hFFFFFFE0;   // Timer mask (should be 0xFFFFFFE0)
+parameter bit [`SCR1_DMEM_AWIDTH-1:0]   SCR1_TIMER_ADDR_PATTERN     = 'hF0040000;   // Timer address match pattern
+
+
+
+`endif // SCR1_ARCH_CUSTOM_SVH
